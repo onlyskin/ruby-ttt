41ce2da3fb510f6d640c82ddccc6b64dcb7913c5
Sam Szreter <s.szreter@gmail.com>
2017-10-09 13:52:22 +0100

    Add accessor [] methods to Board

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,17 @@
 class Board
-    def self.example()
-        0
+    def initialize()
+        @cells = ['-','-','-','-','-','-','-','-','-']
+    end
+
+    def [](key)
+        @cells[key]
+    end
+
+    def []=(key, value)
+        @cells[key] = value
+    end
+
+    def availableSpaces
+        [1, 2, 3, 4, 5, 6, 7, 8, 9]
     end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,10 +1,30 @@
-require "Board"
+require 'Board'
 
 describe Board do
-    describe ".example" do
-        context "given no arguments" do
-            it "returns zero" do
-                expect(Board.example()).to eql(0)
+    describe '[]' do
+        context 'given new board, (3)' do
+            it 'returns "-"' do
+                board = Board.new()
+                expect(board[3]).to eq("-")
+            end
+        end
+    end
+
+    describe '[]=' do
+        context '(7, "X")' do
+            it 'then [] returns "X" for (7)' do
+                board = Board.new()
+                board[7] = "X"
+                expect(board[7]).to eq("X")
+            end
+        end
+    end
+
+    describe '#availableSpaces' do
+        context 'given new board' do
+            it 'returns array of 1 to 9' do
+                board = Board.new() 
+                expect(board.availableSpaces).to eql([1, 2, 3, 4, 5, 6, 7, 8, 9])
             end
         end
     end
