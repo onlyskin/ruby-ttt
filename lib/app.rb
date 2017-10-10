c63d56444df867fcc4041ef6f18c3613c05b4b21
Sam Szreter <s.szreter@gmail.com>
2017-10-10 13:41:12 +0100

    Add run method to board

--- lib/app.rb
+++ lib/app.rb
@@ -1,7 +1,18 @@
+require 'board.rb'
+
 class App
-    def initialize(input=$stdin, output=$stdout)
+    def initialize(input=$stdin, output=$stdout, board=Board.new)
         @output = output
         @input = input
+        @board = board
+    end
+
+    def run
+        while @board.available_moves.length > 0
+            break if @board.game_over?
+            move = input.to_i
+            @board.play(move)
+        end
     end
 
     def output(string)
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,5 +1,21 @@
 require 'App'
 
+class BoardSpy < Board
+    attr_reader :play_called, :play_called_with
+
+    def initialize
+        super()
+        @play_called = false
+        @play_called_with = []
+    end
+
+    def play(move)
+        super(move)
+        @play_called = true
+        @play_called_with.push(move)
+    end
+end
+
 describe App do
     describe 'output' do
         context 'call with string' do
@@ -19,6 +35,29 @@ describe App do
                 output = StringIO.new()
                 app = App.new(input, output)
                 expect(app.input).to eq("test input")
+            end
+        end
+    end
+    describe 'run' do
+        context 'run app' do
+            it 'calls play on BoardSpy with input until no more available moves' do
+                input = StringIO.new("7\n4\n6\n2\n8\n9\n3\n")
+                output = StringIO.new()
+                board_spy = BoardSpy.new()
+                board_spy.play(1)
+                board_spy.play(5)
+                app = App.new(input, output, board_spy)
+                app.run()
+                expect(board_spy.play_called).to be true
+                expect(board_spy.play_called_with).to eq([1, 5, 7, 4, 6, 2, 8, 9, 3])
+            end
+            it 'stops when board.game_over? is true' do
+                input = StringIO.new("1\n2\n4\n5\n7\n")
+                output = StringIO.new()
+                board_spy = BoardSpy.new()
+                app = App.new(input, output, board_spy)
+                app.run()
+                expect(board_spy.play_called_with).to eq([1, 2, 4, 5, 7])
             end
         end
     end
