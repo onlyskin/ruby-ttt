41e3a17284a635a07e96426e5212efabc359e432
Sam Szreter <s.szreter@gmail.com>
2017-10-16 16:48:25 +0100

    Dry up tests

--- spec/move_requester_spec.rb
+++ spec/move_requester_spec.rb
@@ -4,47 +4,39 @@ describe MoveRequester do
   before(:each) do
     @board = instance_double('Board')
     allow(@board).to receive(:available_moves).and_return([6])
+    @ui = instance_double('Ui')
+    allow(@ui).to receive(:output)
+    @move_requester = MoveRequester.new(@ui)
   end
 
   context 'call request' do
     it 'outputs move request message' do
-      ui = instance_double('Ui')
-      allow(ui).to receive(:input).and_return('6')
-      allow(ui).to receive(:output)
-      move_requester = MoveRequester.new(ui)
-      expect(ui).to receive(:output).with(/move/)
-      move_requester.request(@board)
+      set_ui_input(['6'])
+      expect(@ui).to receive(:output).with(/move/)
+      @move_requester.request(@board)
     end
     it 'gets user input from ui' do
-      ui = instance_double('Ui')
-      allow(ui).to receive(:input).and_return('6')
-      allow(ui).to receive(:output)
-      move_requester = MoveRequester.new(ui)
-      expect(ui).to receive(:input)
-      expect(move_requester.request(@board)).to eq(6)
+      set_ui_input(['6'])
+      expect(@ui).to receive(:input)
+      expect(@move_requester.request(@board)).to eq(6)
     end
     it 'rejects string input' do
-      ui = instance_double('Ui')
-      allow(ui).to receive(:input).and_return('invalid', 'still', '6')
-      allow(ui).to receive(:output)
-      move_requester = MoveRequester.new(ui)
-      expect(move_requester.request(@board)).to eq(6)
+      set_ui_input(['invalid', 'still', '6'])
+      expect(@move_requester.request(@board)).to eq(6)
     end
     it 'rejects unavailable move input' do
-      ui = instance_double('Ui')
-      allow(ui).to receive(:input).and_return('100', '8', '6')
-      allow(ui).to receive(:output)
-      move_requester = MoveRequester.new(ui)
+      set_ui_input(['100', '8', '6'])
       expect(@board).to receive(:available_moves)
-      expect(move_requester.request(@board)).to eq(6)
+      expect(@move_requester.request(@board)).to eq(6)
     end
     it 'outputs error message' do
-      ui = instance_double('Ui')
-      allow(ui).to receive(:input).and_return('invalid', '6')
-      allow(ui).to receive(:output)
-      move_requester = MoveRequester.new(ui)
-      expect(ui).to receive(:output).with(/valid input/)
-      expect(move_requester.request(@board)).to eq(6)
+      set_ui_input(['invalid', '6'])
+      expect(@ui).to receive(:output).with(/valid input/)
+      @move_requester.request(@board)
     end
+  end
+
+  def set_ui_input(values)
+    allow(@ui).to receive(:input).and_return(*values)
   end
 end
