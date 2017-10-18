0fa168c470865f0ae422fc14690ff3fa1c79cc0f
Sam Szreter <s.szreter@gmail.com>
2017-10-18 17:30:32 +0100

    Implement game_factory with user input player choice

--- lib/game_factory.rb
+++ lib/game_factory.rb
@@ -1,5 +1,24 @@
+require 'choice_requester'
+
 class GameFactory
-  def self.from_input(game_class)
-    game_class.new(nil, nil)
+  def self.from_input(ui, game_class, choice_hash)
+    choices = self.player_choices(ui, choice_hash)
+    players = choices.map do |choice|
+      self.choice_to_object(choice_hash, choice)
+    end
+    game_class.new(*players)
+  end
+  
+  private
+
+  def self.player_choices(ui, choice_hash)
+    choice_requester = ChoiceRequester.new(ui)
+    (1..2).each.map do |n|
+      choice_requester.request(choice_hash.keys)
+    end
+  end
+
+  def self.choice_to_object(choice_hash, choice)
+    choice_hash.fetch(choice)
   end
 end
--- spec/game_factory_spec.rb
+++ spec/game_factory_spec.rb
@@ -1,15 +1,38 @@
 require 'game_factory'
 require 'game'
+require 'ui'
 
 describe GameFactory do
-  describe 'from_input' do
-    it 'calls initialize on Game' do
+  def Game_from_input(input)
       game_class = class_double('Game')
       allow(game_class).to receive(:new)
+      choice_hash = {'Human' => :human_player,
+        'Computer' => :computer_player}
+      input = StringIO.new(input)
+      output = StringIO.new
+      ui = Ui.new(input, output)
+      GameFactory.from_input(ui, game_class, choice_hash)
+      game_class
+  end
+  describe 'from_input' do
+    it 'calls initialize on Game' do
+      game_class = Game_from_input("1\n1\n")
+      expect(game_class).to have_received(:new)
+    end
+    
+    it 'inits game with two human_players' do
+      game_class = Game_from_input("1\n1\n")
+      expect(game_class).to have_received(:new).with(:human_player, :human_player)
+    end
 
-      GameFactory.from_input(game_class)
+    it 'inits game with two computer players' do
+      game_class = Game_from_input("2\n2\n")
+      expect(game_class).to have_received(:new).with(:computer_player, :computer_player)
+    end
 
-      expect(game_class).to receive(:new)
+    it 'inits game with one human one computer player' do
+      game_class = Game_from_input("1\n2\n")
+      expect(game_class).to have_received(:new).with(:human_player, :computer_player)
     end
   end
 end
