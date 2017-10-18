bb27e348f68f4b315b2e77f10d4728abab5c732e
Sam Szreter <s.szreter@gmail.com>
2017-10-18 10:37:45 +0100

    Use ChoiceRequester to make menu choice in app

--- lib/app.rb
+++ lib/app.rb
@@ -1,16 +1,32 @@
 require_relative 'game'
+require_relative 'minimax'
+require_relative 'computer_player'
+require_relative 'choice_requester'
 
 # A class to manage the running of a game
 class App
   def initialize(ui)
     @ui = ui
+    @choice_requester = ChoiceRequester.new(ui)
+    @MENU_CHOICES = {'Play' => self.method(:run_game),
+                     'Exit' => self.method(:exit)}
   end
 
   def run
     @ui.output('Welcome to Tic Tac Toe')
+    choice = @choice_requester.request(@MENU_CHOICES.keys)
+    @MENU_CHOICES.fetch(choice).call
+  end
+
+  private
+
+  def run_game
     players = [HumanPlayer.new(@ui), HumanPlayer.new(@ui)]
     game = Game.new(@ui, players)
     game.run
     @ui.output('Thanks for playing.')
+  end
+
+  def exit
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -5,7 +5,7 @@ describe App do
   describe 'run' do
     context 'run app with hardcoded human/human game' do
       it 'prints boards out' do
-        input = StringIO.new("1\n2\n4\n5\n7\n")
+        input = StringIO.new("1\n1\n2\n4\n5\n7\n")
         output = StringIO.new
         ui = Ui.new(input, output)
         app = App.new(ui)
@@ -16,7 +16,7 @@ describe App do
       end
 
       it 'prints start and end messages' do
-        input = StringIO.new("1\n2\n4\n5\n20\n7\n")
+        input = StringIO.new("1\n1\n2\n4\n5\n20\n7\n")
         output = StringIO.new
         ui = Ui.new(input, output)
         app = App.new(ui)
@@ -27,6 +27,17 @@ describe App do
         expect(output.string).to include('Thanks')
       end
 
+      it 'prints a game menu' do
+        input = StringIO.new("2\n")
+        output = StringIO.new
+        ui = Ui.new(input, output)
+        app = App.new(ui)
+
+        app.run
+
+        expect(output.string).to include('Play')
+        expect(output.string).to include('Exit')
+      end
     end
   end
 end
