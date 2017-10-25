ee8a15c4602c2df5376c1a8c850af8e97ab531ef
Sam Szreter <s.szreter@gmail.com>
2017-10-25 12:30:41 +0100

    DRY up tests

--- lib/web_app.rb
+++ lib/web_app.rb
@@ -1,10 +1,19 @@
+require_relative 'web_game'
+require_relative 'computer_player'
+require_relative 'minimax'
+
 class WebApp
+  def initialize
+    @game = WebGame.new(ComputerPlayer.new(Minimax.new))
+  end
+
   def call(env)
     req = Rack::Request.new(env)
     if req.post?
       if req.params == {}
         return ['404', {}, []]
       end
+      matrix = @game.board_matrix
       template = File.read('views/game.html.erb')
     else
       template = File.read('views/index.html.erb')
--- spec/web_app_spec.rb
+++ spec/web_app_spec.rb
@@ -4,26 +4,15 @@ require 'rack'
 describe WebApp do
   describe '#call' do
     context 'get' do
-      it 'returns status code 200' do
-        web_app = WebApp.new
-        env = Rack::MockRequest.env_for('', 'REQUEST_METHOD' => 'GET')
-        response = web_app.call(env)
+      it 'status code and content-type correct' do
+        response = response_for_request('GET', '')
         expect(response[0]).to eq('200')
-      end
-
-      it 'returns correct content-type' do
-        web_app = WebApp.new
-        env = Rack::MockRequest.env_for('', 'REQUEST_METHOD' => 'GET')
-        response = web_app.call(env)
         expect(response[1].key?('Content-Type')).to be(true)
         expect(response[1].fetch('Content-Type')).to eq('text/html')
       end
 
       it 'returns html with start button' do
-        web_app = WebApp.new
-        env = Rack::MockRequest.env_for('', 'REQUEST_METHOD' => 'GET')
-        response = web_app.call(env)
-        html = response[2].each.next
+        html = response_for_request('GET', '')[2].each.next
         expect(html).to match(/<form method="post" action="">/)
         expect(html).to match(/<input type="hidden".*name="start".*>/)
         expect(html).to match(/<button type="submit">/)
@@ -31,29 +20,37 @@ describe WebApp do
     end
 
     context 'post with start=true' do
-      it 'returns status code 200' do
-        web_app = WebApp.new
-        env = Rack::MockRequest.env_for('', 'REQUEST_METHOD' => 'POST', :input => 'start=true')
-        response = web_app.call(env)
-        req = Rack::MockRequest.new(env)
+      it 'returns status code 200 and content-type' do
+        response = response_for_request('POST', 'start=true')
         expect(response[0]).to eq('200')
         expect(response[1].key?('Content-Type')).to be(true)
         expect(response[1].fetch('Content-Type')).to eq('text/html')
+      end
+
+      it 'returns html with buttons with empty cells' do
+        response = response_for_request('POST', 'start=true')
         html = response[2].each.next
-        expect(html).to match(/<table>/)
-        expect(html).to match(/<tr>.*<tr>.*<tr>/m)
+        expect(html).to match(/<form method="post" action=""/)
+        expect(html).to match(/<input type="hidden" name="x" value=/)
+        expect(html).to match(/<input type="hidden" name="y" value=/)
+        expect(html).to match(/<button class="board-cell" type="submit"><\/button>/)
+        expect(html).to match(/<button type="submit">/)
+        expect(html).to match(/<input type="hidden".*name="reset".*>/)
       end
     end
     
     context 'post with no data' do
       it 'returns 404' do
-        web_app = WebApp.new
-        env = Rack::MockRequest.env_for('', 'REQUEST_METHOD' => 'POST')
-        response = web_app.call(env)
-        req = Rack::MockRequest.new(env)
+        response = response_for_request('POST', '')
         expect(response[0]).to eq('404')
       end
     end
   end
 
+  def response_for_request(method, data)
+        web_app = WebApp.new
+        env = Rack::MockRequest.env_for('', 'REQUEST_METHOD' => method, :input => data)
+        response = web_app.call(env)
+        response
+  end
 end
