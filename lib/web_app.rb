0ea600de1eeaee93135fb0bb9b7e5760ac7f6c8a
Sam Szreter <s.szreter@gmail.com>
2017-10-26 15:33:48 +0100

    Issue new session cookie on start and on reset

--- lib/web_app.rb
+++ lib/web_app.rb
@@ -4,27 +4,49 @@ require_relative 'minimax'
 
 class WebApp
   def initialize(game)
+    @session_generator = 0
     @game = game
   end
 
   def call(env)
     req = Rack::Request.new(env)
+    if req.get?
+      return get_response(req)
+    end
     if req.post?
+      return post_response(req)
+    end
+  end
+
+  private
+
+  def post_response(req)
       if req.params == {}
         return ['404', {}, []]
       end
+
       if req.params.key?('cell')
         @game.play(req.params['cell'].to_i)
       end
-      if req.params.key?('reset')
-        @game = WebGame.new(ComputerPlayer.new(Minimax.new))
-      end
+
       template = File.read('views/game.html.erb')
-    else
-      template = File.read('views/index.html.erb')
-    end
-    matrix = @game.board_matrix
+      matrix = @game.board_matrix
+      result = ERB.new(template).result(binding)
+      response = Rack::Response.new(result, 200, {'Content-Type' => 'text/html'})
+      if req.params.key?('start') || req.params.key?('reset')
+        response.set_header('session', new_session_id)
+      end
+      response
+  end
+
+  def get_response(req)
+    template = File.read('views/index.html.erb')
     result = ERB.new(template).result(binding)
-    ['200', {'Content-Type' => 'text/html'}, [result]]
+    Rack::Response.new(result, 200, {'Content-Type' => 'text/html'})
+  end
+
+  def new_session_id
+    @session_generator = @session_generator + 1
+    @session_generator.to_s
   end
 end
--- spec/web_app_spec.rb
+++ spec/web_app_spec.rb
@@ -5,45 +5,51 @@ describe WebApp do
   describe '#call' do
     context 'get' do
       it 'status code and content-type correct' do
-        response = response_for_request('GET', '')
-        expect(response[0]).to eq('200')
-        expect(response[1].key?('Content-Type')).to be(true)
-        expect(response[1].fetch('Content-Type')).to eq('text/html')
+        response = @mock_request.get('')
+        expect(response.status).to eq(200)
+        expect(response.get_header('Content-Type')).to eq('text/html')
       end
 
       it 'returns html with start button' do
-        html = response_for_request('GET', '')[2].each.next
-        expect(html).to match(/<form method="post" action="">/)
-        expect(html).to match(/<input type="hidden".*name="start".*>/)
-        expect(html).to match(/<button type="submit">/)
+        response = @mock_request.get('')
+        expect(response.body).to match(/<form method="post" action="">/)
+        expect(response.body).to match(/<input type="hidden".*name="start".*>/)
+        expect(response.body).to match(/<button type="submit">/)
+      end
+
+      it 'doesnt set session cookie' do
+        response = @mock_request.get('')
+        expect(response.has_header?('session')).to be(false)
       end
     end
 
     context 'post with start=true' do
+      it 'sets session cookie' do
+        response = @mock_request.post('', :input => 'start=true')
+        expect(response.get_header('session')).to eq('1')
+      end
+
       it 'returns status code 200 and content-type' do
-        response = response_for_request('POST', 'start=true')
-        expect(response[0]).to eq('200')
-        expect(response[1].key?('Content-Type')).to be(true)
-        expect(response[1].fetch('Content-Type')).to eq('text/html')
+        response = @mock_request.post('', :input => 'start=true')
+        expect(response.status).to eq(200)
+        expect(response.get_header('Content-Type')).to eq('text/html')
       end
 
       it 'returns html with buttons with empty cells' do
-        response = response_for_request('POST', 'start=true')
-        html = response[2].each.next
-        expect(html).to match(/<form method="post" action=""/)
-        expect(html).to match(/<input type="hidden" name="cell" value="9"/)
-        expect(html).to match(/<button class="board-cell" type="submit"><\/button>/)
-        expect(html).to match(/<button type="submit">/)
-        expect(html).to match(/<input type="hidden".*name="reset".*>/)
+        response = @mock_request.post('', :input => 'start=true')
+        expect(response.body).to match(/<form method="post" action=""/)
+        expect(response.body).to match(/<input type="hidden" name="cell" value="9"/)
+        expect(response.body).to match(/<button class="board-cell" type="submit"><\/button>/)
+        expect(response.body).to match(/<button type="submit">/)
+        expect(response.body).to match(/<input type="hidden".*name="reset".*>/)
       end
     end
     
     context 'post with cell data' do
       it 'returns html board with played in cells' do
-        response = response_for_request('POST', 'cell=1')
-        html = response[2].each.next
-        expect(html).to match(/button class="board-cell" type="submit">X<\/button>/)
-        expect(html).to match(/button class="board-cell" type="submit">O<\/button>/)
+        response = @mock_request.post('', :input => 'cell=1')
+        expect(response.body).to match(/button class="board-cell" type="submit">X<\/button>/)
+        expect(response.body).to match(/button class="board-cell" type="submit">O<\/button>/)
       end
     end
 
@@ -58,10 +64,8 @@ describe WebApp do
         web_game.play(6)
         web_game.play(8)
         web_app = WebApp.new(web_game)
-        env = Rack::MockRequest.env_for('', 'REQUEST_METHOD' => 'POST', :input => 'cell=3')
-        response = web_app.call(env)
-        html = response[2].each.next
-        expect(html).to match(/<h1>.*tie.*<\/h1>/)
+        response = Rack::MockRequest.new(web_app).post('', :input => 'cell=3')
+        expect(response.body).to match(/<h1>.*tie.*<\/h1>/)
       end
 
       it 'says that it is a tie in the page' do
@@ -72,41 +76,34 @@ describe WebApp do
         web_game.play(1)
         web_game.play(4)
         web_app = WebApp.new(web_game)
-        env = Rack::MockRequest.env_for('', 'REQUEST_METHOD' => 'POST', :input => 'cell=7')
-        response = web_app.call(env)
-        html = response[2].each.next
-        expect(html).to match(/<h1>.*X.*won.*<\/h1>/)
+        response = Rack::MockRequest.new(web_app).post('', :input => 'cell=7')
+        expect(response.body).to match(/<h1>.*X.*won.*<\/h1>/)
       end
     end
 
     context 'post with reset' do
-      it 'returns html with buttons with empty cells' do
-        response = response_for_request('POST', 'reset=true')
-        html = response[2].each.next
-        expect(html).to match(/<form method="post" action=""/)
-        expect(html).to match(/<input type="hidden" name="cell" value="9"/)
-        expect(html).to match(/<button class="board-cell" type="submit"><\/button>/)
-        expect(html).to match(/<button type="submit">/)
-        expect(html).to match(/<input type="hidden".*name="reset".*>/)
+      it 'sets new session cookie' do
+        response = @mock_request.post('', :input => 'start=true')
+        expect(response.get_header('session')).to eq('1')
+        response = @mock_request.post('', :input => 'reset=true')
+        expect(response.get_header('session')).to eq('2')
       end
     end
 
     context 'post with no data' do
       it 'returns 404' do
-        response = response_for_request('POST', '')
-        expect(response[0]).to eq('404')
+        response = @mock_request.post('')
+        expect(response.status).to eq(404)
       end
     end
   end
 
-  def response_for_request(method, data)
-        minimax = instance_double('Minimax')
-        allow(minimax).to receive(:minimax)
-          .and_return([2, nil], [5, nil])
-        web_game = WebGame.new(ComputerPlayer.new(minimax))
-        web_app = WebApp.new(web_game)
-        env = Rack::MockRequest.env_for('', 'REQUEST_METHOD' => method, :input => data)
-        response = web_app.call(env)
-        response
+  before(:each) do
+    minimax = instance_double('Minimax')
+    allow(minimax).to receive(:minimax)
+      .and_return([2, nil], [5, nil])
+    web_game = WebGame.new(ComputerPlayer.new(minimax))
+    web_app = WebApp.new(web_game)
+    @mock_request = Rack::MockRequest.new(web_app)
   end
 end
