39c46c16f056be62de72dd6cad0ddbc6316de6b6
Sam Szreter <s.szreter@gmail.com>
2017-10-31 16:48:26 +0200

    Add minimax test

--- spec/minimax_spec.rb
+++ spec/minimax_spec.rb
@@ -5,23 +5,24 @@ describe Minimax do
   minimax = Minimax.new
 
   describe 'minimax' do
-    context 'current player has winning move' do
-      it 'X takes winning move' do
-        board = board_from_s('XO-XO----')
-        expect(minimax.minimax(board, nil, 0)[0]).to eq(7)
-      end
-
-      it 'O takes winning move' do
-        board = board_from_s('XX-OO-X--')
-        expect(minimax.minimax(board, nil, 0)[0]).to eq(6)
-      end
+    it 'X takes winning move' do
+      board = board_from_s('XO-XO----')
+      expect(minimax.minimax(board, nil, 0)[0]).to eq(7)
     end
 
-    context 'opposing player would have winning move' do
-      it 'blocks opposing player winning move' do
-        board = board_from_s('X--XO-O--')
-        expect(minimax.minimax(board, nil, 0)[0]).to eq(3)
-      end
+    it 'O takes winning move' do
+      board = board_from_s('XX-OO-X--')
+      expect(minimax.minimax(board, nil, 0)[0]).to eq(6)
+    end
+
+    it 'blocks opposing player winning move' do
+      board = board_from_s('X--XO-O--')
+      expect(minimax.minimax(board, nil, 0)[0]).to eq(3)
+    end
+
+    it 'O plays in center' do
+      board = board_from_s('X--------')
+      expect(minimax.minimax(board, nil, 0)[0]).to eq(5)
     end
   end
 
