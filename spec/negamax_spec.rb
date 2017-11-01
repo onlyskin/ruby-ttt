56303656e38402a8ae0381b65dee5b3a70dd8389
Sam Szreter <s.szreter@gmail.com>
2017-11-01 13:57:27 +0200

    Add a test for 4x4 negamax

--- spec/negamax_spec.rb
+++ spec/negamax_spec.rb
@@ -24,9 +24,14 @@ describe Negamax do
       board = board_from_s('X--------')
       expect(negamax.negamax(board)[0]).to eq(5)
     end
+    
+    it 'can handle a 4x4 game' do
+      board = board_from_s('----------------', size: 4)
+      negamax.negamax(board)
+    end
   end
 
-  def board_from_s(string)
-    Board.new(cells: string.split(''))
+  def board_from_s(string, size: 3)
+    Board.new(cells: string.split(''), size: size)
   end
 end
